fe96b907fae1b47a276e3126ae10256f219c0291
iamShimanto <shimantowebdeveloper@gmail.com>
2026-01-03 11:31:27 +0600

    client add

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/layout/Index";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
